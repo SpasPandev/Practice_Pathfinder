ea68f43efa013e5c0869678b2d00480774dac3dd
SpasPandev <sppan@abv.bg>
2022-06-26 12:16:28 +0300

    Create project

---
+++ src/main/java/com/example/practice_pathfinder/PracticePathfinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_pathfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticePathfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticePathfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practice_pathfinder/PracticePathfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_pathfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticePathfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
